8da3c4d15e9643f28c39297841d933c9f7aca692
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-10-17 14:09:59 +0300

    added User.js

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,14 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static url = '/user';
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
-  static setCurrent(user) {
 
+  static setCurrent(user) {
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   /**
@@ -17,7 +19,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
@@ -25,7 +27,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   /**
@@ -33,7 +35,18 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: `${this.url}/current`,
+      method: 'GET',
+      callback: (err, response) => {
+        if (response.success) {
+          this.setCurrent(response.user);
+        } else {
+            this.unsetCurrent();
+          };
+          callback(err, response);
+      }
+    });
   }
 
   /**
@@ -64,7 +77,17 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: `${this.url}/register`,
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response.success) {
+          this.setCurrent(response.user);
+        };
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -72,6 +95,15 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: `${this.url}/logout`,
+      method: 'POST',
+      callback: (err, response) => {
+        if (response.success) {
+          this.unsetCurrent();
+        };
+        callback(err, response);
+      }
+    });
   }
 }
